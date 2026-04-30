aa49ab4cbfe712509660f3abc5c6dc54021ff847
iconmo <mohammedrabhifilali@gmail.com>
2026-04-30 14:03:59 +0200

    Update sales_order_list.js

--- erpnext_lite_ops/public/js/lists/sales_order_list.js
+++ erpnext_lite_ops/public/js/lists/sales_order_list.js
@@ -1,9 +1,3 @@
-frappe.listview_settings["Sales Order"] = {
-  onload(listview) {
-    if (!window.erpnext_lite_ops) return;
-
-    erpnext_lite_ops.applyListLiteMode(listview, {
-      company_field: "company",
-    });
-  },
-};
+erpnext_lite_ops.extendListViewSettings("Sales Order", {
+  company_field: "company",
+});
