0fb04a3e54503eb0815ebd1dc837a070d51a6659
iconmo <mohammedrabhifilali@gmail.com>
2026-04-30 14:00:56 +0200

    Update purchase_invoice_list.js

--- erpnext_lite_ops/public/js/lists/purchase_invoice_list.js
+++ erpnext_lite_ops/public/js/lists/purchase_invoice_list.js
@@ -1,9 +1,3 @@
-frappe.listview_settings["Purchase Invoice"] = {
-  onload(listview) {
-    if (!window.erpnext_lite_ops) return;
-
-    erpnext_lite_ops.applyListLiteMode(listview, {
-      company_field: "company",
-    });
-  },
-};
+erpnext_lite_ops.extendListViewSettings("Purchase Invoice", {
+  company_field: "company",
+});
